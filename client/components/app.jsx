bd9b3483371b6e40d6c52f97d1b2d932c84ac3b6
seongkevinlee <seongkevinlee@gmail.com>
2020-06-05 23:29:02 -0700

    Added initial state for grades array in App.jsx

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+// import Header from './header';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+  }
+
   render() {
     return null;
   }
