ead516400e74f2e47655c65a4ea6555e608d7226
seongkevinlee <seongkevinlee@gmail.com>
2020-06-06 16:02:29 -0700

    added handleChange event that updates state to value of inputs

--- client/components/grade-form.jsx
+++ client/components/grade-form.jsx
@@ -8,6 +8,21 @@ export default class GradeForm extends React.Component {
       course: '',
       grade: ''
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    if (event.target.id === 'name-input') {
+      this.setState({ name: event.target.value });
+    }
+    if (event.target.id === 'course-input') {
+      this.setState({ course: event.target.value });
+    }
+    if (event.target.id === 'grade-input') {
+      this.setState({ grade: event.target.value });
+    }
+
   }
 
   render() {
@@ -20,7 +35,7 @@ export default class GradeForm extends React.Component {
               <i className='fas fa-user'></i>
             </span>
           </div>
-          <input type='text' className='form-control' placeholder='Name'/>
+          <input type='text' id='name-input' className='form-control' placeholder='Name' value={this.state.name} onChange={this.handleChange}/>
         </div>
 
         <div className='row input-group flex-nowrap'>
@@ -29,7 +44,7 @@ export default class GradeForm extends React.Component {
               <i className='far fa-list-alt'></i>
             </span>
           </div>
-          <input type='text' className='form-control' placeholder='Course'/>
+          <input type='text' id='course-input' className='form-control' placeholder='Course' value={this.state.course} onChange={this.handleChange}/>
         </div>
 
         <div className='row input-group flex-nowrap'>
@@ -38,7 +53,7 @@ export default class GradeForm extends React.Component {
               <i className='fas fa-graduation-cap'></i>
             </span>
           </div>
-          <input type='text' className='form-control' placeholder='Grade'/>
+          <input type='text' id='grade-input' className='form-control' placeholder='Grade' value={this.state.grade} onChange={this.handleChange}/>
         </div>
 
         <div className="row d-inline-flex justify-content-start">
